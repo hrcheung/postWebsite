bf083b30279c4ceb4f9cab959426a326863be28a
hzhang02 <zhang.haoran1@northeastern.edu>
2022-11-28 14:44:00 -0800

    try show mongoDB output in app.js but failed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { title } = require('process')
 const app = express()
 const port = 3100
 
@@ -19,8 +20,31 @@ app.get('/post', (req, res) => {
 
 
 app.get('/mongodb', (req, res) => {
+  const { MongoClient } = require("mongodb");
+
+  // Replace the uri string with your connection string.
+  const uri =
+    "mongodb+srv://m001-student:HfRxQyVrhkR2F0XO@sandbox.jezcu.mongodb.net/?retryWrites=true&w=majority";
   
-  res.send('this is mongodb')
+  const client = new MongoClient(uri);
+  
+  async function run() {
+    try {
+      const database = client.db('sample_mflix');
+      const movies = database.collection('movies');
+  
+      // Query for a movie that has the title 'Back to the Future'
+      const query = { title: 'Back to the Future' };
+      const movie = await movies.findOne(query);
+  
+      console.log(movie);
+      return movie;
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  res.send(run())
 })
 
 app.listen(port, () => {
